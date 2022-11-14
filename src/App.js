e06c4b24625a1659e595988ff66ef16944a0191c
Max <maxpaez.coderhouse@gmail.com>
2022-11-14 12:45:46 -0300

    creación navbar

--- src/App.js
+++ src/App.js
@@ -1,56 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import { ChakraProvider } from '@chakra-ui/react'
+import NavBar from './components/NavBar';
+
 
 function App() {
-  /*Spread operator */
+  let x = "Hola mundo";  
+    return (
+    <div style={{color: "#ff0000"}} className="App">
+      <NavBar />
 
-  // let myobj = { id: 100, nombre: "maxi"};
-  // // myobj.edad = 40;
-
-  // myobj = {...myobj, edad: 40};
-
-  // console.log(myobj)
-
-  //   let myarray = [1, 2, 3, 4 ,5];
-  //   // myarray.push(6) NO SE UTILIZA EN REACT
-  // myarray = [...myarray, 6];
-
-
-  //   console.log(myarray)
-
-  // desestructuración
-  // const persona = {id:100, name: "maxi", edad: 43};
-
-  //   const {name, id} = persona;
-
-  //   console.log(name);
-  //   console.log(id);
-  //   console.log(persona);
-
-
-  function suma(a, b) {
-    return a + b;
-  }
- let resultado = suma(1,3);
-
- console.log(resultado);
- 
-
-
-
-
-
-  return (
-    <ChakraProvider>
-      <div className="App">
-        <h1>Creando un app en React</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Creado por Maximiliano Paez</p>
-
+      <div>
+      <h1>Creando un app en React</h1>
+      <h2>{x}</h2>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Creado por Maximiliano Paez</p>
       </div>
-    </ChakraProvider>
+      
+      <div>
+        Footer
+      </div>
+    </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NavBar = () => {
+  return <div>
+    <ul>
+        <li>
+            <a href="">Link 1</a>
+        </li>
+        <li>
+            <a href="">Link 2</a>
+        </li>
+        <li>
+            <a href="">Link 3</a>
+        </li>
+    </ul>
+  </div>;
+};
+
+export default NavBar;
