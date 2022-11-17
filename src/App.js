5f767c27d2e720889adfc46e658c13283337712e
Max <maxpaez.coderhouse@gmail.com>
2022-11-17 11:44:57 -0300

    ajustes antes de arrancar clase 5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import "./App.css";
-import ItemListContainer from "./components/ItemListContainer";
-import Footer from "./components/Footer";
-import ListadoDeProductosLayout from "./components/ListadoDeProductosLayout";
-import NavBar from "./components/NavBar";
-import Producto from "./components/Producto";
+import './App.css';
+import ItemListContainer from './components/ItemListContainer';
+import Footer from './components/Footer';
+import ListadoDeProductosLayout from './components/ListadoDeProductosLayout';
+import NavBar from './components/NavBar';
+import Producto from './components/Producto';
 
 function App() {
-
-  let producto1 = {id: 100, name: 'Zapatilla Nike', price: 180};
-  let producto2 = {id: 101, name: 'Zapatilla Adidas', price: 150};
-  let producto3 = {id: 102, name: 'Zapatilla Puma', price: 130};
+  let producto1 = { id: 100, name: 'Zapatilla Nike', price: 180 };
+  let producto2 = { id: 101, name: 'Zapatilla Adidas', price: 150 };
+  let producto3 = { id: 102, name: 'Zapatilla Puma', price: 130 };
 
   return (
     <div className="App">
@@ -21,7 +20,7 @@ function App() {
 
       <Producto item={producto1} color={'coral'} />
       <Producto item={producto2} color={'lightblue'} />
-      <Producto item={producto3} color={'coral'} />
+      <Producto item={producto3} color={'coral'} />  
 
       <Footer />
     </div>
